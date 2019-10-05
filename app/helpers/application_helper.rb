e08adc7f45b2cf744cf1ce35d53e485f41ad11e8
スンミン <smlee95kr@gmail.com>
2019-10-05 14:07:19 +0900

    fix issues, caused by incorrect code style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def flash_class(level)
     case level
-      when "notice" then "alert-info"
-      when "alert" then "alert-warning"
-      else "alert-" + level
+    when 'notice' then 'alert-info'
+    when 'alert'  then 'alert-warning'
+    else "alert-#{level}"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
-  delete "/logout", to: "user_sessions#destroy"
+  delete '/logout', to: 'user_sessions#destroy'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: %i[new create]
 end

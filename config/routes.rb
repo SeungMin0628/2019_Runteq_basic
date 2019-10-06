3749994bd85b19f639d77768b78d21d39470e7e1
スンミン <smlee95kr@gmail.com>
2019-10-06 12:51:42 +0900

    change code style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'static_pages#top'
 
-  scope "/:locale" do
+  scope '/:locale' do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'static_pages#top'
 

f2b2f7fda835889e62ae978feb55138bceb68a3b
スンミン <smlee95kr@gmail.com>
2019-10-07 19:53:28 +0900

    change route format

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   get '/', to: 'static_pages#top'
 
-  scope '/:locale' do
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root 'static_pages#top'
+  root 'static_pages#top'
 
-    get '/login', to: 'user_sessions#new'
-    post '/login', to: 'user_sessions#create'
-    delete '/logout', to: 'user_sessions#destroy'
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
 
-    resources :users, only: %i[new create]
-  end
+  resources :users, only: %i[new create]
+
 end

099c2338dbd8dd8d1f49991c6fadff7f6760f7d7
SeungMin0628 <smlee95kr@gmail.com>
2019-10-10 02:24:06 +0900

    fix incorrect redirect-path

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,7 +8,7 @@ class UserSessionsController < ApplicationController
 
     if @user
       # ログイン成功
-      redirect_back_or_to root_path, success: t('flash.success.user_sessions.create')
+      redirect_back_or_to boards_path, success: t('flash.success.user_sessions.create')
     else
       # ログイン失敗
       flash.now[:danger] = t('flash.danger.user_sessions.create')

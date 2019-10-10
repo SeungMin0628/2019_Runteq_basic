a895d42983dbb56960f592372d5d77a27d8ee150
SeungMin0628 <smlee95kr@gmail.com>
2019-10-10 18:57:12 +0900

    add kaminari config

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,5 @@
 class BoardsController < ApplicationController
-  PER = 12
-
   def index
-    @boards = Board.includes(:user).where(['title LIKE ?', "%#{params[:search]}%"]).page(params[:page]).per(PER)
+    @boards = Board.includes(:user).where(['title LIKE ?', "%#{params[:search]}%"]).page(params[:page])
   end
 end

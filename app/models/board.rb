e299e21202d97993eca4c5c8776a2d175d32ad92
SeungMin0628 <smlee95kr@gmail.com>
2019-10-11 12:53:26 +0900

    fix wrong validations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,7 @@
 class Board < ApplicationRecord
   # Validations
-  validates :title, confirmation: true, length: { maximum: 255 }
-  validates :body, confirmation: true, length: { maximum: 65535 }
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, presence: true, length: { maximum: 65535 }
 
   # Relations
   belongs_to :user

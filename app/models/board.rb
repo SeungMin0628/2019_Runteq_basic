3c6d1f0215e5ea8a60f657fa12f99a338d55a4e0
SeungMin0628 <smlee95kr@gmail.com>
2019-10-12 18:07:21 +0900

    delete validation boards image

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,7 +5,6 @@ class Board < ApplicationRecord
   # Validations
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65535 }
-  validates :board_image, presence: true
 
   # Relations
   belongs_to :user

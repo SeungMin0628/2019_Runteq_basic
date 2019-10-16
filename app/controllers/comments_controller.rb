9d530a9db015d7510e9a9e35e70d4d801912baac
SeungMin0628 <smlee95kr@gmail.com>
2019-10-17 01:14:19 +0900

    change code style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(
-      :body,
+      :body
     ).merge(
       board_id: params[:board_id]
     )

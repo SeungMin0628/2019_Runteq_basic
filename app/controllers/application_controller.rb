93624639b7b4ad5b5c6dffd1e521cd31c27c4176
SeungMin0628 <smlee95kr@gmail.com>
2019-10-19 14:47:35 +0900

    delete not used resque exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   around_action :switch_locale
   before_action :require_login
   add_flash_types :success, :info, :warning, :danger
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
@@ -21,11 +19,5 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     flash[:danger] = t('flash.danger.user_sessions.need_login')
     redirect_to login_path
-  end
-
-  def render_404(exception = nil)
-    logger.info "Rendering 404 with exception: #{exception.message}" if exception
-
-    render file: "#{Rails.root}/public/404.html", status: 404
   end
 end

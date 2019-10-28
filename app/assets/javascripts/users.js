0ac2d1d16b3ddcd53136a4906c6b442b59e9622c
SeungMin0628 <smlee95kr@gmail.com>
2019-10-28 20:14:31 +0900

    [Fix] fix js variable type

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 $(function() {
   function readURL(input) {
     if (input.files && input.files[0]) {
-      let reader = new FileReader();
+      const reader = new FileReader();
 
       reader.onload = function (e) {
         $('#js-avatar_prev').attr('src', e.target.result);

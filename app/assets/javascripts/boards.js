a45a88496c7f53b9b69c034771856481a810a72d
SeungMin0628 <smlee95kr@gmail.com>
2019-10-28 20:01:59 +0900

    [Fix] fix code, js variable type

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -1,7 +1,7 @@
 $(function() {
   function readURL(input) {
     if (input.files && input.files[0]) {
-      var reader = new FileReader();
+      let reader = new FileReader();
 
       reader.onload = function (e) {
         $('#js-board_image_prev').attr('src', e.target.result);

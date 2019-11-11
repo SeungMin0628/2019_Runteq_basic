3992a522612134c8c70caef2f2829060c241a22d
SeungMin0628 <smlee95kr@gmail.com>
2019-11-11 22:03:50 +0900

    [Modify] refactor sessions controller

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Admin::SessionsController < Admin::AdminController
   skip_before_action :require_login, only: %i[new create]
-  before_action :authenticated, except: %i[destroy]
   layout 'admin_before_login'
 
   def new; end
@@ -9,10 +8,6 @@ class Admin::SessionsController < Admin::AdminController
     @user = login(params[:email], params[:password])
 
     if @user
-      unless @user.has_role? :admin
-        return redirect_to root_path, warning: t('flash.warning.admin.sessions.has_no_role')
-      end
-
       # login success => go to dashboards
       redirect_back_or_to admin_root_path, success: t('flash.success.user_sessions.create')
     else
@@ -25,13 +20,5 @@ class Admin::SessionsController < Admin::AdminController
   def destroy
     logout
     redirect_to admin_login_path, success: t('flash.success.user_sessions.destroy')
-  end
-
-  private
-
-  def authenticated
-    if logged_in?
-      return redirect_to admin_root_path if current_user.has_role? :admin
-    end
   end
 end

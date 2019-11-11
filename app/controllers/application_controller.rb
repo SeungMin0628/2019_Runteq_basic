329ade2b05dc55df775d821a8db74e04a7fab777
SeungMin0628 <smlee95kr@gmail.com>
2019-11-11 21:51:44 +0900

    [Modify] refactor application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < ActionController::Base
   # redefinition of 'sorcery#not_authenticated'
   # ログインしなかったユーザーがアクセスした際、警告メッセージを表示しログイン画面に遷移させる
   def not_authenticated
-    flash[:danger] = t('flash.danger.user_sessions.need_login')
-    redirect_to login_path
+    redirect_to login_path, danger: t('flash.danger.user_sessions.need_login')
   end
 end

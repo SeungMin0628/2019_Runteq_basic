f27fa8d5e4e1aa11a0ce11c2d7fcacc7eb4ff54c
SeungMin0628 <smlee95kr@gmail.com>
2019-11-18 21:19:13 +0900

    [Fix] refactor not required comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,9 @@
 class Comment < ApplicationRecord
-  # Validations
   validates :body, presence: true, length: { maximum: 65535 }
 
-  # Relations
   belongs_to :user
   belongs_to :board
 
-  # Instance method
   def owned_by?(user)
     user_id == user&.id
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,29 +5,22 @@ class User < ApplicationRecord
   after_create :assign_default_role
   mount_uploader :avatar, AvatarUploader
 
-  # Validations
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
   validates :last_name, :first_name, presence: true
 
-  # Relations
   has_many :boards, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :bookmarked_boards, through: :bookmarks, class_name: :Board
   has_many :comments, dependent: :destroy
 
-  # Scope
   scope :id_desc, -> { order(id: :desc) }
 
   def bookmark_for(board)
     bookmarks.find_by(board_id: board.id)
   end
-
-  # PRIVATE methods
 
   private
 

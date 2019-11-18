8004ba87b7b6b10c2049f875d711ee83d87fe51e
SeungMin0628 <smlee95kr@gmail.com>
2019-11-18 20:47:54 +0900

    [Modify] delete not required comment

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -8,8 +8,6 @@ class Admin::AdminController < ApplicationController
     return redirect_to(root_path, warning: t('flash.warning.admin.sessions.has_no_role')) unless current_user.has_role? :admin
   end
 
-  # redefinition of 'sorcery#not_authenticated'
-  # ログインしなかったユーザーがアクセスした際、警告メッセージを表示しログイン画面に遷移させる
   def not_authenticated
     redirect_to admin_login_path, danger: t('flash.danger.user_sessions.need_login')
   end
